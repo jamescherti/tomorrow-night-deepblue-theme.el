808d4506ee34b9a2b5e591ecd59c4861435d6db0
James Cherti <60946298+jamescherti@users.noreply.github.com>
2024-05-31 08:45:14 -0400

    Added a large number of faces to the Emacs theme.

--- tomorrow-night-deepblue-theme.el
+++ tomorrow-night-deepblue-theme.el
@@ -1,3 +1,37 @@
-;;; GNU Emacs port of the Tomorrow Theme Deepblue -*- lexical-binding: t -*-
+;;; tomorrow-night-deepblue-theme.el --- Tomorrow Night Deepblue -*- lexical-binding: t -*-
+
+;;; Tomorrow Night Deepblue
+;;
+;; Maintainer: James Cherti
+;; URL: https://github.com/jamescherti/emacs-tomorrow-night-deepblue
+;; Keywords: faces, theme, accessibility
+
+;; This file is NOT part of GNU Emacs.
+
+;; This file is free software; you can redistribute it and/or modify it under
+;; the terms of the GNU General Public License as published by the Free
+;; Software Foundation; either version 2, or (at your option) any later
+;; version.
+
+;; This file is distributed in the hope that it will be useful, but WITHOUT ANY
+;; WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+;; FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+;; details.
+
+;; You should have received a copy of the GNU General Public License along with
+;; GNU Emacs. If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;;
+;; The Emacs theme Tomorrow Night Deepblue is a beautiful deep blue variant of
+;; the Tomorrow Night theme, which is renowned for its elegant color
+;; palette that is pleasing to the eyes.
+;;
+;; The Tomorrow Night Deepblue features a deep blue background color that
+;; creates a calming atmosphere. The contrasting colors make it easy to
+;; distinguish between different elements of your code. The
+;; tomorrow-night-deepblue theme is also a great choice for programmer who miss
+;; the blue themes that were trendy a few years ago.
+
 (require 'base-tomorrow-night-deepblue)
 (color-theme-tomorrow--define-theme night-deepblue)
