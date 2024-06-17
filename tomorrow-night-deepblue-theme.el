24c1c1e2e2ed907ddaa0382c2dbd5ebe2db8d198
James Cherti <60946298+jamescherti@users.noreply.github.com>
2024-06-17 13:02:30 -0400

    Update headers

--- tomorrow-night-deepblue-theme.el
+++ tomorrow-night-deepblue-theme.el
@@ -6,7 +6,7 @@
 ;; URL: https://github.com/jamescherti/tomorrow-night-deepblue-theme.el
 ;; Package-Requires: ((emacs "26.1"))
 ;; Keywords: faces themes
-;; Version: 1.0.0
+;; Version: 1.1.0
 
 ;; This file is NOT part of GNU Emacs.
 
