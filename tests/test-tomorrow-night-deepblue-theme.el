de7de2c7f14a979a721d6dc7400d3f547ee2ec3f
James Cherti <60946298+jamescherti@users.noreply.github.com>
2024-08-17 14:18:47 -0400

    Add Cask, Makefile, tests, and GitHub actions

---
+++ tests/test-tomorrow-night-deepblue-theme.el
@@ -0,0 +1,36 @@
+;;; test-tomorrow-night-deepblue-theme.el --- Test tomorrow-night-deepblue-theme -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2024 James Cherti | https://www.jamescherti.com/contact/
+
+;; Author: James Cherti
+;; URL: https://github.com/jamescherti/tomorrow-night-deepblue-theme.el
+;; Package-Requires: ((emacs "26.1"))
+;; Keywords: faces themes
+;; Version: 1.1.2
+;; SPDX-License-Identifier:  GPL-3.0-or-later
+
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
+
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;; Test tomorrow-night-deepblue-theme.
+
+;;; Code:
+
+(defun test-tomorrow-night-deepblue-theme ()
+  "Test tomorrow-night-deepblue-theme."
+  (interactive)
+  t)
+
+(provide 'test-tomorrow-night-deepblue-theme)
+;;; test-tomorrow-night-deepblue-theme.el ends here
